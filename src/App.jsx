7a943982c412d59bc5421740bbcdeabdb6f87cbf
Shivam Patel <shivampatel0048@gmail.com>
2024-06-15 20:12:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div className='mt-20 text-center'>
+        <h1 className='text-4xl font-bold'>Hello, World!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
